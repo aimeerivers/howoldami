b79c0aaf39c40c762dd29b6a4a8472abad87fd1d
aimee rivers <aimee@aimeerivers.com>
2024-09-04 22:10:49 +0200

    style: apply linting

--- js/script.js
+++ js/script.js
@@ -1,34 +1,34 @@
-const resultElement = document.getElementById('result');
-const ageElement = document.getElementById('age');
-let previousAge = '';
+const resultElement = document.getElementById("result");
+const ageElement = document.getElementById("age");
+let previousAge = "";
 
 const params = new URLSearchParams(window.location.search);
 
 try {
-  const dob = params.get('dob');
+  const dob = params.get("dob");
   if (dob) {
     const parsedDate = new Date(dob);
     if (isNaN(parsedDate)) {
-      throw new Error('Invalid date format.');
+      throw new Error("Invalid date format.");
     }
     parsedDate.setHours(0, 0, 0, 0);
 
     updateAge(parsedDate);
-    resultElement.style.display = 'block';
+    resultElement.style.display = "block";
 
     setInterval(() => {
       updateAge(parsedDate);
     }, 1000);
 
   } else {
-    throw new Error('dob not found.');
+    throw new Error("dob not found.");
   }
 } catch (error) {
-  const formElement = document.getElementById('form');
+  const formElement = document.getElementById("form");
   if (formElement) {
-    formElement.style.display = 'block';
+    formElement.style.display = "block";
   } else {
-    console.log('Form element not found.');
+    console.log("Form element not found.");
   }
 }
 
@@ -38,23 +38,23 @@ function updateAge(dob) {
   const ageInYears = ageInMilliseconds / (1000 * 60 * 60 * 24 * 365.25);
   const ageInYearsRounded = ageInYears.toFixed(7);
 
-  ageElement.innerHTML = ''; // Clear previous content
+  ageElement.innerHTML = ""; // Clear previous content
 
   // Split the age into individual digits
-  const digits = ageInYearsRounded.split('');
+  const digits = ageInYearsRounded.split("");
   digits.forEach((digit, index) => {
-    const span = document.createElement('span');
-    span.className = 'digit';
+    const span = document.createElement("span");
+    span.className = "digit";
     if (!isNaN(digit)) {
-      span.classList.add('card');
+      span.classList.add("card");
     }
     span.textContent = digit;
 
     // Apply flip animation only if the digit has changed
     if (previousAge[index] !== digit) {
-      const randomDuration = (Math.random() * (0.5 - 0.05) + 0.05).toFixed(2) + 's';
+      const randomDuration = (Math.random() * (0.5 - 0.05) + 0.05).toFixed(2) + "s";
       span.style.animationDuration = randomDuration;
-      span.classList.add('slide');
+      span.classList.add("slide");
     }
 
     ageElement.appendChild(span);
